d40dfa9e1a2d9b30cb4cc5941da2b6b5373108de
Jorge Moreira <jorge.moreira.96@gmail.com>
2026-04-23 16:29:44 +0100

    fix: Split app to different bundles and lazy load (#8)
    - Split app to different bundles
    - Lazy load bundles to maximize performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './hooks/useTheme';
 import Layout from './components/Layout';
-import Home from './pages/Home';
-import CV from './pages/CV';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 import './App.css';
+
+const Home = lazy(() => import('./pages/Home'));
+const CV = lazy(() => import('./pages/CV'));
+const Projects = lazy(() => import('./pages/Projects'));
+const Contact = lazy(() => import('./pages/Contact'));
 
 function App() {
   return (
@@ -13,10 +15,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="cv" element={<CV />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="contact" element={<Contact />} />
+            <Route index element={<Suspense><Home /></Suspense>} />
+            <Route path="cv" element={<Suspense><CV /></Suspense>} />
+            <Route path="projects" element={<Suspense><Projects /></Suspense>} />
+            <Route path="contact" element={<Suspense><Contact /></Suspense>} />
           </Route>
         </Routes>
       </BrowserRouter>
